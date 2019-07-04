fdae77277c145821cef162d9b7ebda7571e18211
SteveNelson83 <51002138+SteveNelson83@users.noreply.github.com>
2019-07-04 13:31:00 +0100

    set sail function added

--- _tests_/Ship.test.js
+++ _tests_/Ship.test.js
@@ -1,9 +1,17 @@
 /* globals it describe expect */
-const Ship = require('..src/Ship');
+const Ship = require('../src/Ship.js');
 
 describe('Ship', () => {
+    it('can be instantiated', () => {
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
     it('has a starting port', () => {
         const ship = new Ship ('Dover');
         expect(ship.startingPort).toBe('Dover');
+    });
+    it('can set sail', () => {
+        const ship = new Ship('Dover');
+        ship.setSail();
+        expect(ship.startingPort).toBeFalsy();
     });
 })
--- src/Ship.js
+++ src/Ship.js
@@ -2,4 +2,10 @@ function Ship (startingPort) {
     this.startingPort = startingPort;
 }
 
+Ship.prototype ={
+    setSail: function () {
+        this.startingPort = 0;
+    }
+};
+
 module.exports = Ship;

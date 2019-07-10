541ac539fedf15ff140e90c448adc92d6b82f5ee
SteveNelson83 <51002138+SteveNelson83@users.noreply.github.com>
2019-07-10 13:48:49 +0100

    made ship test suit DRY

--- _tests_/Ship.test.js
+++ _tests_/Ship.test.js
@@ -4,40 +4,34 @@ const Port = require('../src/Port.js');
 const Itinerary = require ('../src/Itinerary.js');
 
 describe('Ship', () => {
+    let ship;
+    let dover;
+    let calais;
+    let itinerary;
+    beforeEach(() => {
+        dover = new Port('Dover');
+        calais = new Port('Calais');
+        itinerary = new Itinerary([dover, calais]);
+        ship = new Ship(itinerary);
+    });
     it('can be instantiated', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
         expect(new Ship(itinerary)).toBeInstanceOf(Object);
     });
     it('has a starting port', () => {
-        const port = new Port ('Dover');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
-        expect(ship.currentPort).toBe(port);
+        expect(ship.currentPort).toBe(dover);
     });
     it('can set sail', () => {
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ship = new Ship(itinerary);
         ship.setSail();
         expect(ship.currentPort).toBeFalsy();
         expect(dover.ships).not.toContain(ship);
     });
     it('can dock at a different port', () => {
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ship = new Ship(itinerary);
         ship.setSail();
         ship.dock();
         expect(ship.currentPort).toBe(calais);
         expect(calais.ships).toContain(ship);
     });
     it('gets added to port on instantiation', () => {
-        const dover = new Port('Dover');
-        const itinerary = new Itinerary([dover]);
-        const ship = new Ship(itinerary);
         expect(dover.ships).toContain(ship);
     });
 })

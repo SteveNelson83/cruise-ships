77fab5348a8f662fc05d46a8e46f7af4b0d2c137
SteveNelson83 <51002138+SteveNelson83@users.noreply.github.com>
2019-07-15 17:59:23 +0100

    final commit

--- _tests_/Ship.test.js
+++ _tests_/Ship.test.js
@@ -1,10 +1,7 @@
 /* globals it describe expect */
 const Ship = require('../src/Ship.js');
-const Port = require('../src/Port.js');
-const Itinerary = require ('../src/Itinerary.js');
 
 describe('Ship', () => {
-    let port
     let ship;
     let dover;
     let calais;
@@ -23,8 +20,10 @@ describe('Ship', () => {
             ...port,
             name: 'Calais',
             ships: []
-        }
-        itinerary = new Itinerary([dover, calais]);
+        };
+        itinerary = {
+            ports: [dover, calais]
+        };
         ship = new Ship(itinerary);
     });
     it('can be instantiated', () => {

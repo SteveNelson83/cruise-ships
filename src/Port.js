07636c988c197a4ff2b6dee2dc576be3b9549b85
SteveNelson83 <51002138+SteveNelson83@users.noreply.github.com>
2019-07-26 03:51:39 +0100

    added export functions to all constructors

--- src/Port.js
+++ src/Port.js
@@ -1,19 +1,24 @@
-function Port (name) {
-    this.name = name;
-    this.ships = [];
-}
+(function exportPort() {
+    function Port (name) {
+        this.name = name;
+        this.ships = [];
+    }
 
-Port.prototype = {
-    addShip: function (ship) {
-        this.ships.push(ship);
-    },
-    removeShip: function (ship) {
-        for ( var i = 0; i < this.ships.length; i++) { 
-            if ( this.ships[i] === ship) {
-              this.ships.splice(i, 1); 
+    Port.prototype = {
+        addShip: function (ship) {
+            this.ships.push(ship);
+        },
+        removeShip: function (ship) {
+            for ( var i = 0; i < this.ships.length; i++) { 
+                if ( this.ships[i] === ship) {
+                this.ships.splice(i, 1); 
+                }
             }
-        }
-    },
-};    
-
-module.exports = Port;
+        },
+    };
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Port;
+    } else {
+        window.Port = Port;
+    }
+}());
--- src/controller.js
+++ src/controller.js
@@ -1,13 +1,20 @@
-function Controller () {
-    this.initialiseSea();
-}
-Controller.prototype.initialiseSea = function initialiseSea() {
-    const backgrounds = [
-        './images/water0.png',
-        './images/water1.png',
-    ];
-    let backgroundIndex = 0;
-    window.setInterval(() => {
-        document.querySelector('#viewport').getElementsByClassName.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
-    }, 1000);
-};
+(function exportController() {
+    function Controller () {
+            this.initialiseSea();
+        }
+        Controller.prototype.initialiseSea = function initialiseSea() {
+            const backgrounds = [
+                './images/water0.png',
+                './images/water1.png',
+            ];
+            let backgroundIndex = 0;
+            window.setInterval(() => {
+                document.querySelector('#viewport').getElementsByClassName.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+            }, 1000);
+        };
+        if (typeof module !== 'undefined' && module.exports) {
+            module.exports = Controller;
+        } else {
+            window.Controller = Controller;
+        }
+}());

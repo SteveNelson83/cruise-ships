ac61caf8b3aad3093af0085efa065bd62e73d1f4
SteveNelson83 <51002138+SteveNelson83@users.noreply.github.com>
2019-07-26 04:41:24 +0100

    added port visuals

--- src/controller.js
+++ src/controller.js
@@ -2,16 +2,31 @@
     function Controller () {
             this.initialiseSea();
         }
-        Controller.prototype.initialiseSea = function initialiseSea() {
-            const backgrounds = [
-                './images/water0.png',
-                './images/water1.png',
-            ];
-            let backgroundIndex = 0;
-            window.setInterval(() => {
-                document.querySelector('#viewport').getElementsByClassName.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
-            }, 1000);
-        };
+        Controller.prototype = {
+            initialiseSea: function () {
+                const backgrounds = [
+                    './images/water0.png',
+                    './images/water1.png',
+                ];
+                let backgroundIndex = 0;
+                window.setInterval(() => {
+                    document.querySelector('#viewport').getElementsByClassName.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+                }, 1000);
+            },
+            renderPorts: function (ports) {
+                const portsElement = document.querySelector('#ports');
+                portsElement.style.width = '0px';
+                ports.forEach((port, index) => {
+                    const newPortElement = document.createElement('div');
+                    newPortElement.dataset.portName = port.name;
+                    newPortElement.dataset.portIndex = index;
+                    newPortElement.className = 'port';
+                    portsElement.appendChild(newPortElement);
+                    const portsElementWidth = parseInt(portsElement.style.width, 10);
+                    portsElement.style.width = `${portsElementWidth + 256}px`;
+                })
+            },
+        };  
         if (typeof module !== 'undefined' && module.exports) {
             module.exports = Controller;
         } else {
